0492c77146351497490adf2fba0d8230fa3b4072
Mark King <mark@vemek.co>
2021-08-15 18:53:42 +0100

    remove iterm, alacritty all the way

--- .config/yadm/iterm2/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-. "$HOME/.config/yadm/util.sh"
-
-if [ "$SYSTEM_TYPE" = "Darwin" ]; then
-  log_info "Configuring iTerm2"
-  defaults write com.googlecode.iterm2 PrefsCustomFolder "$HOME/.config/iterm2"
-fi
